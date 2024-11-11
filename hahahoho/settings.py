cfedea571a3c0297a026b8af2461815c3a227c60
박가희 <930_10@naver.com>
2024-11-12 02:20:09 +0900

    Fix: 환경변수 설정

--- hahahoho/settings.py
+++ hahahoho/settings.py
@@ -26,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['wish.com', '']
+DEBUG = True
 
 
 # Application definition
