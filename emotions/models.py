dde903b1765bf852998b8e104b4b115a89221ec8
박가희 <930_10@naver.com>
2024-11-18 21:30:29 +0900

    Feat: 감정기록 생성,수정일 필드 추가, 관심사 추가 시 관심사 모델에도 같이 등록

--- emotions/models.py
+++ emotions/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+class Interest(models.Model):
+    member_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    interests = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Emotion(models.Model):
     member_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     mission_content = models.TextField()
@@ -20,9 +29,15 @@ class Emotion(models.Model):
     relational = models.IntegerField()
     refusing = models.IntegerField()
     essential = models.IntegerField()
-
-
-class Interest(models.Model):
-    member_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    interests = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save, sender=Emotion)
+def create_interest(sender, instance, created, **kwargs):
+    if created:
+        # interest_keyword를 기반으로 Interest 객체 생성
+        interest = Interest.objects.create(
+            member_id=instance.member_id,
+            interests=instance.interest_keyword
+        )
